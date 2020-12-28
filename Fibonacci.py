7edb18b16b36ddb3e7a2f524d6eee645a79445b1
reemahs0pr0 <shameer_93@hotmail.com>
2020-12-28 20:17:55 +0800

    dynamic programming

--- Fibonacci.py
+++ Fibonacci.py
@@ -15,27 +15,50 @@ def num_ways_recursion(feet):
         return 1
     else:
         return num_ways_recursion(feet-1) + num_ways_recursion(feet-2)
+    
+def num_ways_memoized(feet, memo):
+    if feet in range(2):
+        return 1
+    else:
+        if memo[feet] != None:
+            return memo[feet]
+        else:
+            memo[feet] = 0
+            memo[feet] = num_ways_memoized(feet-1, memo) + \
+                        num_ways_memoized(feet-2, memo)
+            return memo[feet]
 
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-n = np.arange(0, 34)
+n = np.arange(0, 100)
 bu_time = []
 r_time = []
+memo_time = []
 
 for num in n:
     start1 = time.time()
     num_ways_bottom_up(num)
     bu_time.append(time.time() - start1)
-    start2 = time.time()
-    num_ways_recursion(num)
-    r_time.append(time.time() - start2)
+    
+    if num <= 20:
+        start2 = time.time()
+        num_ways_recursion(num)
+        r_time.append(time.time() - start2)
+    else:
+        r_time.append(None)
+    
+    memo = [None] * (num+1)
+    start3 = time.time()
+    num_ways_memoized(num, memo)
+    memo_time.append(time.time() - start3)
     
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(n, bu_time, color='red', linewidth=1, marker='o', label='bottom_up')
 ax.plot(n, r_time, color='green', linewidth=1, marker='o', label='recursion')
-plt.legend(loc='upper left')
+ax.plot(n, memo_time, color='blue', linewidth=1, marker='o', label='memoized')
+plt.legend(loc='upper right')
 ax.set(title='Feet', ylabel='Time')
 plt.show()
