3a5081ca584333cc11f1abf7847fbd46a87033e1
reemahs0pr0 <shameer_93@hotmail.com>
2021-06-06 21:14:54 +0800

    Update BinaryTree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -27,18 +27,41 @@ def serialize(node):
     return serial
 
 def deserialize(serial):
-    arr = serial.split()
-    index = 0
+    arr = sort(serial)
+    root = Node('root')
     for i in range(len(arr)):
-        if arr[i] == 'root':
-            index = i
-            break
-    root = Node(arr[i])
-    for i in range(index+1, len(arr)):
-        root.insert_right(arr[i])
-    for i in range(index-1, -1, -1):
-        root.insert_left(arr[i])
+        if (arr[i] == 'root'):
+            continue
+        val_arr = arr[i].split('.')
+        if (len(val_arr) == 1 and val_arr[0] == 'left'):
+            root.insert_left('left')
+        elif (len(val_arr) == 1 and val_arr[0] == 'right'):
+            root.insert_right('right')
+        else:
+            base = root
+            for j in range(len(val_arr)-1):
+                if (val_arr[j] == 'left'):
+                    root = root.left
+                else:
+                    root = root.right
+                if (j == len(val_arr)-2):
+                    if (val_arr[len(val_arr)-1] == 'left'):
+                        root.insert_left(arr[i])
+                    else:
+                        root.insert_right(arr[i])
+                    root = base
     return root
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
+def sort(serial):
+    arr = serial.split()
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if len(arr[j]) > len(arr[j+1]):
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+    return arr
+
+node = Node('root', Node('left', Node('left.left'), Node('left.right')), Node('right', Node('right.left'), Node('right.right')))
+print(serialize(node))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
