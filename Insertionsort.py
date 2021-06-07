49c684f8fa94ebe000bd631b5703bbde36c7978f
reemahs0pr0 <shameer_93@hotmail.com>
2021-06-07 21:33:31 +0800

    Update Insertionsort.py

--- Insertionsort.py
+++ Insertionsort.py
@@ -4,7 +4,6 @@ def insertionsort(arr):
     for i in range(1, len(arr)):
         j = i-1
         elem = arr[i]
-        
         while arr[j] > elem and j >= 0:
             arr[j+1] = arr[j]
             j -= 1
